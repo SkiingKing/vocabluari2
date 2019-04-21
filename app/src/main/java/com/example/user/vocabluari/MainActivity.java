9d1f3c31796eba34a072810f757b21ede125822b
SkiingKing <45917637+SkiingKing@users.noreply.github.com>
2019-04-21 17:46:00 +0300

    uxuxuxuxu

--- app/src/main/java/com/example/user/vocabluari/MainActivity.java
+++ app/src/main/java/com/example/user/vocabluari/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.user.vocabluari;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,13 +11,15 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
 
     private static int RESULT = 0 ;
-    private Button answer_one,answer_two,answer_three;
+    private Button answer_one,answer_two,answer_three,next;
     private TextView right_word,result;
     public ArrayList<String> value;
     private ArrayList transete;
+    private String rightWord;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,37 +30,78 @@ public class MainActivity extends AppCompatActivity {
         answer_two = findViewById(R.id.answer_two);
         answer_three = findViewById(R.id.answer_three);
         right_word = findViewById(R.id.right_word);
-        result = findViewById(R.id .test_result);
-
-
-
+        result = findViewById(R.id.test_result);
+        next = findViewById(R.id.next);
 
         Vocabluari group_one = new Vocabluari();
 
         value = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.word_value)));
         transete = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.word_translete)));
 
-        group_one.work(value,transete,right_word,answer_one,answer_two,answer_three);
+        group_one.work(value, transete, right_word, answer_one, answer_two, answer_three);
+
+
+
+
+
+
+        rightWord = group_one.work(value, transete, right_word, answer_one, answer_two, answer_three);
 
         answer_one.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View view) {
-                RESULT++;
-
+                if(rightWord == answer_one.getText()){
+                    RESULT++;
+                    answer_one.setBackgroundColor(Color.GREEN);
+                    String r = Integer.toString(RESULT);
+                    result.setText(r);
+                }else answer_one.setBackgroundColor(Color.RED);
             }
         });
-        String r = Integer.toString(RESULT);
-        result.setText(r);
 
+        answer_two.setOnClickListener(new View.OnClickListener() {
 
-        if((answer_one.isPressed()) || (answer_two.isPressed()) || (answer_three.isPressed())){
-          group_one.work(value,transete,right_word,answer_one,answer_two,answer_three);
-          Log.d("Pizdez","Pizdez");
-        }
+            @SuppressLint("ResourceType")
+            @Override
+            public void onClick(View view) {
+                if(rightWord == answer_two.getText()){
+                    RESULT++;
+                    answer_two.setBackgroundColor(Color.GREEN);
+                    String r = Integer.toString(RESULT);
+                    result.setText(r);
+                }else answer_two.setBackgroundColor(Color.RED);
+            }
+        });
 
+        answer_three.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("ResourceType")
+            @Override
+            public void onClick(View view) {
+                if(rightWord == answer_three.getText()){
+                    RESULT++;
+                    answer_three.setBackgroundColor(Color.GREEN);
+                    String r = Integer.toString(RESULT);
+                    result.setText(r);
+                }else answer_three.setBackgroundColor(Color.RED);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Vocabluari next_vob = new  Vocabluari();
+                next_vob.work(value, transete, right_word, answer_one, answer_two, answer_three);
+            }
+        });
 
     }
-}
+
+    }
+
+
 
 
 
